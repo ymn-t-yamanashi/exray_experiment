60185de3d884b00421016e7430c1a33c85fc47ab
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2025-01-03 06:37:04 +0900

    初期化 ビルド失敗

---
+++ lib/exray_experiment.ex
@@ -0,0 +1,18 @@
+defmodule ExrayExperiment do
+  @moduledoc """
+  Documentation for `ExrayExperiment`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExrayExperiment.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/exray_experiment_test.exs
@@ -0,0 +1,8 @@
+defmodule ExrayExperimentTest do
+  use ExUnit.Case
+  doctest ExrayExperiment
+
+  test "greets the world" do
+    assert ExrayExperiment.hello() == :world
+  end
+end
